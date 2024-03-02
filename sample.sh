9ac0b9d7c4979dd41748349157d53528733b2ed3
tajuddin <tajuddin.n@sonata-software.com>
2024-03-02 12:29:39 +0530

    initial commit

---
+++ sample.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "This is my sample shell script"
+echo "My shell script name is $0"
+echo "My hostname is: $(hostname)"
+echo "Print disk space"
+df -h
+echo "create a directory"
+mkdir directory1
