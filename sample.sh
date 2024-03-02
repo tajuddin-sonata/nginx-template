966a573d508e95643edb828fe45c651dc866b848
tajuddin <tajuddin.n@sonata-software.com>
2024-03-02 12:55:53 +0530

    initial commit

--- sample.sh
+++ sample.sh
@@ -5,4 +5,14 @@ echo "My hostname is: $(hostname)"
 echo "Print disk space"
 df -h
 echo "create a directory"
-mkdir directory1
+
+
+directory="directory1"
+
+if [ -d "$directory" ]; then
+    echo "Directory $directory already exists. Removing it..."
+    rm -rf "$directory"
+fi
+
+echo "Creating directory $directory"
+mkdir -p "$directory"
